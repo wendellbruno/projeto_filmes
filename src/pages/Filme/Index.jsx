e43c6d718c88dc659013a518272efea9f5edd920
wendellbruno <79750052+wendellbruno@users.noreply.github.com>
2023-06-04 09:52:43 -0300

    commit inicial
    commit inicial

---
+++ src/pages/Filme/Index.jsx
@@ -0,0 +1,10 @@
+
+function Filme(){
+    return (
+        <div>
+            <h1>Filmes</h1>
+        </div>
+    )
+}
+
+export default Filme;
---
+++ src/pages/Home/Index.jsx
@@ -0,0 +1,10 @@
+
+function Home(){
+    return (
+        <div>
+            <h1>HOME</h1>
+        </div>
+    )
+}
+
+export default Home;
